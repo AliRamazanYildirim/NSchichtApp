3af59a5da3a6d13ed374bf890c2333c84e8e2e65
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-04-20 16:04:18 +0300

    Datenbankkontext in der Anwendung installieren

---
+++ NSchicht.Kern/Quellen/IGenerischeQuelle.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+
+namespace NSchicht.Kern.Quellen
+{
+    public interface IGenerischeQuelle<T> where T : class
+    {
+        Task<T> GehZurIDAsync(int ID);
+        IQueryable<T> GehZurAlleDaten(Expression<Func<T, bool>> expression);
+        IQueryable<T> Wo(Expression<Func<T, bool>> expression);
+        Task<bool> IrgendeinAsync(Expression<Func<T, bool>> expression);
+        Task InsertAsync(T einheit);
+        Task InsertFeldAsync(IEnumerable<T> einheiten);
+        void Aktualisieren();
+        void Entfernen();
+        void EntfernenFeld(IEnumerable<T> einheiten);
+
+
+    }
+}
---
+++ NSchicht.Quelle/AppDbKontext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NSchicht.Kern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSchicht.Quelle
+{
+    public class AppDbKontext:DbContext
+    {
+        public AppDbKontext(DbContextOptions<AppDbKontext> options):base(options)
+        {
+
+        }
+        public DbSet<Kategorie> Kategorien { get; set; }
+        public DbSet<Produkt> Produkte { get; set; }
+        public DbSet<ProduktEigenschaft> ProduktEigenschaften { get; set; }
+    }
+}
