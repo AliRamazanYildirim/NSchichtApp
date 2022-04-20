06fcad388926587137ac58ac9bf44c7b44e20802
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-04-20 22:44:14 +0300

    Produkt Controller erstellen

---
+++ NSchicht.API/Controllers/ProdukteController.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NSchicht.Kern;
+using NSchicht.Kern.Dienste;
+using NSchicht.Kern.DÜOe;
+
+namespace NSchicht.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdukteController : BenutzerDefinierteBasisController
+    {
+        private readonly IMapper _mapper;
+        private readonly IDienst<Produkt> _dienst;
+
+        public ProdukteController(IDienst<Produkt> dienst, IMapper mapper)
+        {
+            _dienst = dienst;
+            _mapper = mapper;
+        }
+        //GET api/produkte
+        [HttpGet]
+        public async Task<IActionResult>Alle()
+        {
+            var produkte=await _dienst.GehZurAlleDatenAsync();
+            var produkteDüoe = _mapper.Map<List<ProduktDüo>>(produkte.ToList());
+            return CreateActionResult(BenutzerDefinierteAntwortDüo<List<ProduktDüo>>.Erfolg(200, produkteDüoe));
+        }
+        //GET api/produkte/7
+        [HttpGet("{ID}")]
+        public async Task<IActionResult> GehZurID(int ID)
+        {
+            var produkt = await _dienst.GehZurIDAsync(ID);
+            var produkteDüo = _mapper.Map<ProduktDüo>(produkt);
+            return CreateActionResult(BenutzerDefinierteAntwortDüo<ProduktDüo>.Erfolg(200, produkteDüo));
+        }
+        [HttpPost]
+        public async Task<IActionResult> Speichern(ProduktDüo produktDüo)
+        {
+            var produkt = await _dienst.InsertAsync(_mapper.Map<Produkt>(produktDüo));
+            var produkteDüo = _mapper.Map<ProduktDüo>(produkt);
+            return CreateActionResult(BenutzerDefinierteAntwortDüo<ProduktDüo>.Erfolg(201, produkteDüo));
+        }
+        [HttpPut]
+        public async Task<IActionResult> Aktualisieren(ProduktAktualisierenDüo produktDüo)
+        {
+            await _dienst.AktualisierenAsync(_mapper.Map<Produkt>(produktDüo));
+            return CreateActionResult(BenutzerDefinierteAntwortDüo<KeinInhaltDüo>.Erfolg(204));
+        }
+        [HttpDelete("{ID}")]
+        public async Task<IActionResult> Entfernen(int ID)
+        {
+            var produkt = await _dienst.GehZurIDAsync(ID);
+             await _dienst.EntfernenAsync(produkt);
+            return CreateActionResult(BenutzerDefinierteAntwortDüo<KeinInhaltDüo>.Erfolg(204));
+        }
+    }
+}
---
+++ NSchicht.API/Controllers/benutzerDefinierteBasisController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NSchicht.Kern.DÜOe;
+
+namespace NSchicht.API.Controllers
+{
+    
+    public class BenutzerDefinierteBasisController : ControllerBase
+    {
+        [NonAction]
+        public IActionResult CreateActionResult<T>(BenutzerDefinierteAntwortDüo<T> antwort)
+        {
+            if (antwort.StatusCode == 204)
+                return new ObjectResult(null)
+                {
+                    StatusCode = antwort.StatusCode
+                };
+            return new ObjectResult(antwort)
+            {
+                StatusCode = antwort.StatusCode
+            };
+        }
+    }
+}
