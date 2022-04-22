d105d7c702a96625233fe603c71b75b70f0cb2bc
AliRamazanYildirim <94836230+AliRamazanYildirim@users.noreply.github.com>
2022-04-22 22:23:46 +0300

    Produkt Controller Speichern Teil 1

--- NSchicht.Web/Controllers/ProdukteController.cs
+++ NSchicht.Web/Controllers/ProdukteController.cs
@@ -1,22 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NSchicht.Kern;
 using NSchicht.Kern.Dienste;
+using NSchicht.Kern.DÜOe;
 
 namespace NSchicht.Web.Controllers
 {
     public class ProdukteController : Controller
     {
         private readonly IProduktDienst _produktDienst;
-
-        public ProdukteController(IProduktDienst produktDienst)
+        private readonly IKategorieDienst _kategorieDienst;
+        private readonly IMapper _mapper;
+        public ProdukteController(IProduktDienst produktDienst, IKategorieDienst kategorieDienst, IMapper mapper)
         {
             _produktDienst = produktDienst;
+            _kategorieDienst = kategorieDienst;
+            _mapper = mapper;
         }
 
-        
+
         public async Task<IActionResult> Index()
         {
             
             return View(await _produktDienst.RufProdukteMitKategorie());
         }
+        public async Task<IActionResult> Speichern()
+        {
+            var kategorien =  _kategorieDienst.GehZurAlleDatenAsync();
+            var kategorienDüoe = _mapper.Map<List<KategorieDüo>>(kategorien);
+            ViewBag.kategorien = new SelectList(kategorienDüoe, "ID", "Name");
+
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Speichern(ProduktDüo produktDüo)
+
+        {
+            if (ModelState.IsValid)
+            {
+
+                await _produktDienst.InsertAsync(_mapper.Map<Produkt>(produktDüo));
+                return RedirectToAction(nameof(Index));
+            }
+            var kategorien = _kategorieDienst.GehZurAlleDatenAsync();
+            var kategorienDüoe = _mapper.Map<List<KategorieDüo>>(kategorien);
+            ViewBag.kategorien = new SelectList(kategorienDüoe, "ID", "Name");
+            return View();
+        }
+
     }
 }
--- NSchicht.Web/Program.cs
+++ NSchicht.Web/Program.cs
@@ -1,7 +1,29 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using NSchicht.Dienst.Kartierungen;
+using NSchicht.Quelle;
+using NSchicht.Web.Modules;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddAutoMapper(typeof(KartierungsProfil));
+builder.Services.AddDbContext<AppDbKontext>(x =>
+{
+    x.UseSqlServer(builder.Configuration.GetConnectionString("Sqlconnection"), option =>
+    {
+        option.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbKontext)).GetName().Name);
+    }
+    );
+});
+
+builder.Host.UseServiceProviderFactory
+    (new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
+containerBuilder.RegisterModule(new QuelleDienstModule()));
 
 var app = builder.Build();
 
