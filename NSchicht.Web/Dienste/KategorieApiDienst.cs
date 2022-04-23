ab9e6ef1d84bcdd847aa26f430559d87006594b1
AliRamazanYildirim <94836230+AliRamazanYildirim@users.noreply.github.com>
2022-04-23 07:35:31 +0300

    Produkt APi Dienst  / Kategorie Api Dienst definieren

---
+++ NSchicht.Web/Dienste/KategorieApiDienst.cs
@@ -0,0 +1,12 @@
+﻿namespace NSchicht.Web.Dienste
+{
+    public class KategorieApiDienst
+    {
+        private readonly HttpClient _httpClient;
+
+        public KategorieApiDienst(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+    }
+}
---
+++ NSchicht.Web/Dienste/ProduktApiDienst.cs
@@ -0,0 +1,12 @@
+﻿namespace NSchicht.Web.Dienste
+{
+    public class ProduktApiDienst
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProduktApiDienst(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+    }
+}
